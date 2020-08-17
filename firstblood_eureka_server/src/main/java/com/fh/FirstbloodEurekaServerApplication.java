97535fc07cd23827e6730acd3f07459822f0ed23
ljy <1249094460@qq.com>
2020-08-17 21:50:42 +0800

    修改测试1

--- firstblood_eureka_server/src/main/java/com/fh/FirstbloodEurekaServerApplication.java
+++ firstblood_eureka_server/src/main/java/com/fh/FirstbloodEurekaServerApplication.java
@@ -10,5 +10,5 @@ public class FirstbloodEurekaServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(FirstbloodEurekaServerApplication.class, args);
     }
-    //aaaaadsf
+    //aaaaadsfsdf
 }
