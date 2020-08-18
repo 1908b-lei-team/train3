c0117386c05ed4aeafc835db68afee7da429deab
wangyy-0512 <69137646+wangyy-0512@users.noreply.github.com>
2020-08-18 21:19:19 +0800

    创建 ControlMapper.java

---
+++ firstblood_borrowmoney/src/main/java/com/fh/mapper/ControlMapper.java
@@ -0,0 +1,9 @@
+package com.fh.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.fh.model.Control;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ControlMapper extends BaseMapper<Control> {
+}
