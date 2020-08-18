46a4c2983cdb001e78c4a665a12497f1d4ede5dc
hsl <1017032237@qq.com>
2020-08-18 20:18:42 +0800

    更新 FirstbloodHslApplication.java
    hsl123

--- firstblood_hsl/src/main/java/com/fh/FirstbloodHslApplication.java
+++ firstblood_hsl/src/main/java/com/fh/FirstbloodHslApplication.java
@@ -9,5 +9,5 @@ public class FirstbloodHslApplication {
     public static void main(String[] args) {
         SpringApplication.run(FirstbloodHslApplication.class, args);
     }
-//aaa
+//aaa1234
 }
