7c5825123e62cf94e2d2d3ebb5847411def111d4
hsl <1017032237@qq.com>
2020-08-18 19:11:08 +0800

    惠三磊测试

---
+++ firstblood_hsl/src/main/java/com/fh/FirstbloodHslApplication.java
@@ -0,0 +1,13 @@
+package com.fh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstbloodHslApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstbloodHslApplication.class, args);
+    }
+
+}
