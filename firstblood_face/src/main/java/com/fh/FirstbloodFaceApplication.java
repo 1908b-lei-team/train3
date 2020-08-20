36f52d73a7c5772e11380e0bd630764cbfdb2292
never-ljy <63041195+never-ljy@users.noreply.github.com>
2020-08-20 16:49:18 +0800

    加入网关
    雷金源

--- firstblood_face/src/main/java/com/fh/FirstbloodFaceApplication.java
+++ firstblood_face/src/main/java/com/fh/FirstbloodFaceApplication.java
@@ -3,14 +3,16 @@ package com.fh;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 //@MapperScan({"com.itboyst.facedemo.dao.mapper","com.itboyst.facedemo.mapper"})
 @EnableTransactionManagement
-public class Application {
+@EnableEurekaClient
+public class FirstbloodFaceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(FirstbloodFaceApplication.class, args);
     }
 }
 
