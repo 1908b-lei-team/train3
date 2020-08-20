2e738c28f9b5cb320c791a35ce31f2eb67d33d96
sunrj <sunrongjing0081@163.com>
2020-08-20 15:13:42 +0800

    sunrj

--- firstblood_login/src/main/java/com/p2p/service/UserLoginService.java
+++ firstblood_login/src/main/java/com/p2p/service/UserLoginService.java
@@ -1,4 +1,12 @@
 package com.p2p.service;
 
+import com.p2p.model.User;
+import com.p2p.util.ServerResponse;
+
 public interface UserLoginService {
+    void register(User user);
+
+    ServerResponse queryByUserPhone(String userPhone);
+
+    ServerResponse userLogin(User user);
 }
